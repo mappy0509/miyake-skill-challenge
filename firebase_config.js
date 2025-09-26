8c3a36dc9d9060c264771ef5c5a37b373895bc73
masahiro yamamoto <masahiroyamamoto@masahironoMacBook-Air.local>
2025-09-26 18:46:56 +0900

    完成バージョンリリース

---
+++ firebase_config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCD6IMEwHvKz8ibzAK-RILL4peePYj45sA",
+  authDomain: "dunks-skill-challenge.firebaseapp.com",
+  projectId: "dunks-skill-challenge",
+  storageBucket: "dunks-skill-challenge.appspot.com",
+  messagingSenderId: "188351050553",
+  appId: "1:188351050553:web:fc1841ca230a59b36b2ee8"
+};
